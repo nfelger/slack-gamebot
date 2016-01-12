29442134a4150130245742155c09f4e5e1cfefd6
Niko Felger <niko.felger@gmail.com>
2016-01-12 17:28:35 +0100

    wip

--- slack-gamebot/commands/record.rb
+++ slack-gamebot/commands/record.rb
@@ -2,11 +2,28 @@ module SlackGamebot
   module Commands
     class Record < SlackRubyBot::Commands::Base
       def self.call(client, data, match)
-        #challenger = ::User.find_create_or_update_by_slack_id!(client, data.user)
-        #arguments = match['expression'].split.reject(&:blank?) if match.names.include?('expression')
-        #arguments ||= []
+        arguments = match['expression'].split.reject(&:blank?) if match.names.include?('expression')
+        arguments ||= []
+        verbs = ['beat', 'crushed', 'destroyed', 'humiliated', 'obliterated', 'defeated', 'conquered', 'vanquished', 'trounced', 'routed', 'overwhelmed', 'quashed', 'clobbered', 'pwned']
+        unless (arguments.size == 3 and verbs.include?(arguments[1]))
+            client.say(channel: data.channel, text: "I don't understand")
+            return nil
+        end
+
+        winner = ::User.find_by_slack_mention!(client, arguments[0])
+        loser = ::User.find_by_slack_mention!(client, arguments[2])
+        Challenge.create!(
+          team: team,
+          channel: channel,
+          created_by: winner,
+          challengers: [winner],
+          challenged: [loser],
+          state: ChallengeState::ACCEPTED
+        )
+        challenge.lose!(loser)
+
         client.say(channel: data.channel, text: match.inspect, gif: 'noted')
-        logger.info "record: #{data.user} - #{}"
+        logger.info "record: #{data.user} - #{arguments}"
       end
     end
   end
